fc76cbd9df9306d69b633d1d32681ee35ada2540
andysd-com <andy@andysd.com>
2025-08-27 22:57:20 -0400

    Initialize 1CryptKeep Express app

---
+++ server.js
@@ -0,0 +1,6 @@
+﻿const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (_, res) => res.send("1CryptKeep is live!"));
+app.listen(port, () => console.log(`Listening on ${port}`));
